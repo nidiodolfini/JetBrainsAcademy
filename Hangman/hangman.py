9b880b8499464ccae807194bd7e624bad5e5dc9f
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-28 15:36:13 -0300

    hangman 4/8

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,15 @@
 # Write your code here
-word = 'python'
-guess_word = input()
+import random
+
+word = random.choice(['python', 'java', 'kotlin', 'javascript'])
+hidden_word = word[:3] + '-' * (len(word)-3)
+# for i in range(len(word)):
+#     if i < 3:
+#         hidden_word += word[i]
+#     else:
+#         hidden_word += '-'
+print('H A N G M A N')
+guess_word = input(f'Guess the word {hidden_word}:')
 if guess_word == word:
     print('You survived!')
 else:
