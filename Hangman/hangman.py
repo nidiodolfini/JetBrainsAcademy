044deec8b6e754346393f85dca13ae66e0ed7f12
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-29 19:03:08 -0300

    update hangman to stage 7/8 no completed yet
    If the user enters the same letter twice then the program should output You already typed this letter .
    Also, you should check if the user prints an English lowercase letter or not. If not, the program should print It is not an ASCII lowercase letter .
    Also, you should check if the user printed exactly one letter. If not, the program should print You should print a single letter . Remember that zero is also not one!
    Note that all these three errors should not reduce attempts count!

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,38 +1,37 @@
 # Write your code here
 import random
+from string import ascii_lowercase
 
 word = random.choice(['python', 'java', 'kotlin', 'javascript'])
 hidden_word = '-' * (len(word))
 show_word = list(hidden_word)
+tried_letter = list()
 counter = 0
 print('H A N G M A N')
-print()
+
 while counter < 8 and word != ''.join(show_word):
+    print()
     print(''.join(show_word))
     guess_letter = input(f'Input a letter: ')
-    if guess_letter in word:
+    if (guess_letter in word) and (guess_letter in ascii_lowercase) and (len(guess_letter) == 1):
         if guess_letter in show_word:
-            print('No improvements')
-            counter += 1
-            if counter < 8:
-                print()
+            print('You already typed this letter')
         else:
             for p in range(len(word)):
                 if guess_letter == word[p]:
                     show_word[p] = guess_letter
-            if counter < 8:
-                print()
-
+    elif len(guess_letter) < 1 or len(guess_letter) > 1:
+        print('You should print a single letter')
+    elif (guess_letter not in ascii_lowercase) and (len(guess_letter) == 1):
+        print('It is not an ASCII lowercase letter')
+    elif guess_letter in tried_letter:
+        print('You already typed this letter')
     else:
         print('No such letter in the word')
         counter += 1
-        if counter < 8:
-            print()
-
+    tried_letter += guess_letter
 if ''.join(show_word) == word:
-    print()
-    print(''.join(show_word))
-    print('You guessed the word!')
+    print(f'You guessed the {word}!')
     print('You survived!')
 else:
     print('You are hanged!')
