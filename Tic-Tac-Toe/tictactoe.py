fd188b038ab57184c6882d2fc77743468fd91598
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-03-01 22:00:29 -0300

    Create tictactoe.py
    done 2/5 Tic-Tac-Toe project

---
+++ Tic-Tac-Toe/tictactoe.py
@@ -0,0 +1,8 @@
+# write your code here
+position = input()
+
+print('---------')
+print(f'| {position[0]} {position[1]} {position[2]} |')
+print(f'| {position[3]} {position[4]} {position[5]} |')
+print(f'| {position[6]} {position[7]} {position[8]} |')
+print('---------')
