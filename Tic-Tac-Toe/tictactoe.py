a3da014c2c716c367971a4ddc0d9669e9c115dab
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-03-02 22:16:53 -0300

    Update tictactoe.py

--- Tic-Tac-Toe/tictactoe.py
+++ Tic-Tac-Toe/tictactoe.py
@@ -27,12 +27,46 @@ print('---------')
 under = [entrada for entrada in position if entrada == '_']
 xis = [entrada for entrada in position if entrada == 'X']
 os = [entrada for entrada in position if entrada == 'O']
-print(under, xis, os)
+# print(under, xis, os)
 if position[0] == position[1] == position[2] and position[3] == position[4] == position[5]:
     print('Impossible')
 elif position[0] == position[1] == position[2] and position[6] == position[7] == position[8]:
     print('Impossible')
 elif position[3] == position[4] == position[5] and position[6] == position[7] == position[8]:
     print('Impossible')
-if len(under) > 2:
+elif position[0] == position[3] == position[6] and position[1] == position[4] == position[7]:
+    print('Impossible')
+elif position[3] == position[4] == position[5] and position[2] == position[5] == position[8]:
+    print('Impossible')
+elif position[3] == position[4] == position[5] and position[2] == position[5] == position[8]:
+    print('Impossible')
+elif len(xis) - len(os) >= 2 or len(os) - len(xis) >= 2:
+    print('Impossible')
+
+elif position[0] == position[1] == position[2]:
+    print(f'{position[0]} wins')
+elif position[3] == position[4] == position[5]:
+    print(f'{position[0]} wins')
+elif position[6] == position[7] == position[8]:
+    print(f'{position[0]} wins')
+elif position[0] == position[4] == position[8]:
+    print(f'{position[0]} wins')
+elif position[6] == position[4] == position[2]:
+    print(f'{position[2]} wins')
+elif position[0] == position[3] == position[6]:
+    print(f'{position[2]} wins')
+elif position[1] == position[4] == position[7]:
+    print(f'{position[2]} wins')
+elif position[2] == position[5] == position[8]:
+    print(f'{position[2]} wins')
+elif len(under) >= len(xis) or len(under) >= len(os) and position[0] != position[1]:
     print('Game not finished')
+elif len(under) >= len(xis) or len(under) >= len(os) and position[4] != position[5]:
+    print('Game not finished')
+elif len(under) >= len(xis) or len(under) >= len(os) and position[7] != position[8]:
+    print('Game not finished')
+elif len(under) >= len(xis) or len(under) >= len(os) and position[0] != position[1]:
+    print('Game not finished')
+
+else:
+    print('Draw')
