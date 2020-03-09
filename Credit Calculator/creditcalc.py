9fd800b2cf1cf3d2485671ef16ca598d50266d45
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-03-09 17:35:38 -0300

    Update creditcalc.py
    fazendo 4/4, feito as verificações que devem dar erro

--- Credit Calculator/creditcalc.py
+++ Credit Calculator/creditcalc.py
@@ -1,41 +1,54 @@
 import math
+import sys
+import argparse
+
+args = sys.argv
+values_positive = True
 
 
-def enter_value(some_list):
-    return_list = []
-    for value in some_list:
-        print(value)
-        return_list.append(float(input('> ')) if len(return_list) < 2 else (float(input('> ')) / 1200))
-    return return_list
+def diff(args):
+    print('diff', args)
 
 
-a_list = ['Enter credit principal:', 'Enter count of period:', 'Enter credit interest:']
-n_list = ['Enter credit principal:', 'Enter monthly payment:', 'Enter credit interest:']
-p_list = ['Enter monthly payment:', 'Enter count of period:', 'Enter credit interest:']
-print('What do you want to calculate?\n'
-      'type "n" - for count of months,\n'
-      'type "a" - for annuity monthly payment,\n'
-      'type "p" - for monthly payment:')
-calculate_option = input('> ')
+def annuity(args):
+    print('annuity', args)
 
-if calculate_option == 'p':
-    monthly_payment, periods, interest = enter_value(p_list)
-    pow_i_n = math.pow(1 + interest, periods)
-    principal = math.floor(monthly_payment / ((interest * pow_i_n) / (pow_i_n - 1)))
-    print(f'Your credit principal = {principal}')
 
-if calculate_option == 'a':
-    principal, periods, interest = enter_value(a_list)
-    pow_i_n = math.pow(1 + interest, periods)
-    annuity_payment = math.ceil(principal * ((interest * pow_i_n) / (pow_i_n - 1)))
-    print(f'Your annuity payment = {annuity_payment}')
+def check_positive(value):
+    return_value = int(value)
+    if return_value <= 0:
+        global values_positive
+        values_positive = False
 
-if calculate_option == 'n':
-    principal, monthly_payment, interest = enter_value(n_list)
-    month_count = math.ceil(math.log(monthly_payment / (monthly_payment - interest * principal), (1 + interest)))
-    months = month_count % 12
-    years = month_count // 12
-    text = f'{months} month{"s" if months != 1 else ""} to replay the credit'
-    if years > 0:
-        text = f'{years} year{"s" if years != 1 else ""} and ' + text
-    print(f'It takes ' + text)
+    return return_value
+
+
+def check_positive_float(value):
+    return_value = float(value)
+    if return_value <= 0:
+        global values_positive
+        values_positive = False
+    return return_value
+
+
+if len(args) == 5:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-t", "--type", required=True)
+    ap.add_argument("-p", "--principal", type=check_positive)
+    ap.add_argument("-i", "--interest", type=check_positive_float)
+    ap.add_argument("-m", "--payment", type=check_positive)
+    ap.add_argument("-mo", "--periods", type=check_positive)
+    args = vars(ap.parse_args())
+    # print(ap)
+    # print(len(args))
+    if values_positive and not (args['type'] == 'diff' and args['payment'] is not None) and args['interest'] is not None:
+        if args['type'] == 'annuity':
+            annuity(args)
+        elif args['type'] == 'diff':
+            diff(args)
+        else:
+            print('Incorrect parameters')
+    else:
+        print('Incorrect parameters')
+else:
+    print('Incorrect parameters')
