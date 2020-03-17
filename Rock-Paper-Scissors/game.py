fb711d42bb27d8be105691b92b60c5ba3a90329a
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-03-17 13:51:56 -0300

    doing 3/4 Rock-Paper-Scissors project

---
+++ Rock-Paper-Scissors/game.py
@@ -0,0 +1,72 @@
+# Write your code here
+from random import choice
+jokenpo = ''
+rating = []
+player = {}
+point = 0
+file = open('rating.txt', 'r', encoding='utf-8')
+lista = file.read().split('\n')
+# print(lista)
+for v,i in enumerate(lista):
+    if len(i) > 1:
+        player["name"], player["point"] = i.split(" ")
+        print(player["name"], player["point"])
+        rating.append(player.copy())
+        player.clear()
+
+
+file.close()
+# print(len(lista))
+name = input("Enter your name: ")
+
+if len(lista) < 2:
+    player['name'] = name
+    player['point'] = 0
+    rating.append(player.copy())
+    player.clear()
+for i in range(len(rating)):
+    if (rating[i]["name"]) == name:
+        point += int(rating[i]["point"])
+        break
+    else:
+        # print("entou else")
+        player['name'] = name
+        player['point'] = 0
+        rating.append(player.copy())
+        player.clear()
+        break
+print(f'Hello, {name}')
+while True:
+    computer = choice(['rock', 'scissors', 'paper'])
+    jokenpo = input(' ')
+    if jokenpo == "!exit":
+        break
+    if jokenpo == computer:
+        print(f'There is a draw ({computer})')
+        point += 50
+    elif jokenpo == 'scissors' and computer == 'paper':
+        print(f'Well done. Computer chose {computer} and failed')
+        point += 100
+    elif jokenpo == 'paper' and computer == 'rock':
+        print(f'Well done. Computer chose {computer} and failed')
+        point += 100
+    elif jokenpo == 'rock' and computer == 'scissors':
+        print(f'Well done. Computer chose {computer} and failed')
+        point += 100
+    elif jokenpo == "!rating":
+        print(f'Your rating: {point}')
+    else:
+        print(f'Sorry, but computer chose {computer}')
+print('Bye!')
+
+for i in range(len(rating)):
+    if (rating[i]["name"]) == name:
+        rating[i]["point"] = point
+
+file = open('rating.txt', 'w', encoding='utf-8')
+for i in range(len(rating)):
+    file.write(str(rating[i]["name"]))
+    file.write(" ")
+    file.write(str(rating[i]["point"]))
+    file.write("\n")
+file.close()
