8605e05b8c8c41a81856b2353f594d834550e447
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-03-17 22:03:13 -0300

    project Rock-Paper-Scissors done 5/5

--- Rock-Paper-Scissors/game.py
+++ Rock-Paper-Scissors/game.py
@@ -23,7 +23,7 @@ def default_game():
 
         if jokenpo == "!exit":
             break
-        if jokenpo == computer:
+        elif jokenpo == computer:
             print(f'There is a draw ({computer})')
             point += 50
         elif jokenpo == 'scissors' and computer == 'paper':
@@ -37,6 +37,62 @@ def default_game():
             point += 100
         elif jokenpo == "!rating":
             print(f'Your rating: {point}')
+        else:
+            print(f'Sorry, but computer chose {computer}')
+    print('Bye!')
+
+def novo():
+    global point
+    while True:
+        computer = choice(options)
+        # print(computer)
+
+        try:
+            jokenpo = input(' ')
+        except (EOFError):
+            break #end of file reached
+
+
+        # print(jokenpo)
+        if jokenpo == "!exit":
+            break
+
+        if jokenpo == "!rating":
+            print(f'Your rating: {point}')
+        elif jokenpo == computer:
+            print(f'There is a draw ({computer})')
+            point += 50
+        elif jokenpo in options_one and computer in options_one:
+            # print('options one')
+            if options_one.index(jokenpo) > options_one.index(computer):
+                print(f'Well done. Computer chose {computer} and failed')
+                point += 100
+            else:
+                print(f'Sorry, but computer chose {computer}')
+        elif jokenpo in options_two and computer in options_two:
+            # print("options two")
+            if options_two.index(jokenpo) > options_two.index(computer):
+                print(f'Well done. Computer chose {computer} and failed')
+                point += 100
+            else:
+                print(f'Sorry, but computer chose {computer}')
+        elif jokenpo in options_one and computer in options_two:
+            # print('options one and two')
+            print(f'Well done. Computer chose {computer} and failed')
+            point += 100
+        elif jokenpo in options_mid and computer in options_one:
+            print(f'Well done. Computer chose {computer} and failed')
+            point += 100
+        elif jokenpo in options_mid and computer in options_two:
+            print(f'Sorry, but computer chose {computer}')
+
+        # elif jokenpo == 'paper' and computer == 'rock':
+        #     print(f'Well done. Computer chose {computer} and failed')
+        #     point += 100
+        # elif jokenpo == 'rock' and computer == 'scissors':
+        #     print(f'Well done. Computer chose {computer} and failed')
+        #     point += 100
+
         else:
             print(f'Sorry, but computer chose {computer}')
     print('Bye!')
@@ -66,51 +122,26 @@ options = input().split(',')
 print("Okay, let's start")
 if len(options) < 2:
     default_game()
-# print(options)
-options_one = options[:round(len(options)/2+0.5)]
-options_two = options[round(len(options)/2+0.5):]
+
+elif len(options) > 5:
+    options_one = options[:-round(len(options)/2)]
+    options_mid = options[-(round(len(options)/2))]
+    # print(round(len(options)/2))
+    options_two = options[-round(len(options)/2)+1:]
+    novo()
+else:
+    options_one = options[:round(len(options) / 2)]
+    options_mid = options[(round(len(options) / 2))]
+    # print(round(len(options)/2))
+    options_two = options[round(len(options) / 2) + 1:]
+    novo()
 # print(options_one)
+# print(options_mid)
 # print(options_two)
-while True:
-    computer = choice(options)
-    # print(computer)
-    jokenpo = input(' ')
-    if jokenpo == "!exit":
-        break
-    if jokenpo == computer:
-        print(f'There is a draw ({computer})')
-        point += 50
-    if jokenpo == "!rating":
-        print(f'Your rating: {point}')
-    elif jokenpo in options_one and computer in options_one:
-        # print('options one')
-        if options_one.index(jokenpo) > options_one.index(computer):
-            print(f'Well done. Computer chose {computer} and failed')
-            point += 100
-        else:
-            print(f'Sorry, but computer chose {computer}')
-    elif jokenpo in options_two and computer in options_two:
-        # print("options two")
-        if options_two.index(jokenpo) > options_two.index(computer):
-            print(f'Well done. Computer chose {computer} and failed')
-            point += 100
-        else:
-            print(f'Sorry, but computer chose {computer}')
-    elif jokenpo in options_one and computer in options_two:
-        # print('options one and two')
-        print(f'Well done. Computer chose {computer} and failed')
-        point += 100
 
-    # elif jokenpo == 'paper' and computer == 'rock':
-    #     print(f'Well done. Computer chose {computer} and failed')
-    #     point += 100
-    # elif jokenpo == 'rock' and computer == 'scissors':
-    #     print(f'Well done. Computer chose {computer} and failed')
-    #     point += 100
 
-    else:
-        print(f'Sorry, but computer chose {computer}')
-print('Bye!')
+
+
 
 for i in range(len(rating)):
     if (rating[i]["name"]) == name:
